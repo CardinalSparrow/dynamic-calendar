f445ff0eed7a7b9970320619b22553fb6f38c752
cardinalsparrow <cardinalsparrow@gmail.com>
2023-09-04 23:47:15 +0100

    updated the calendar

--- script.js
+++ script.js
@@ -1,40 +1,61 @@
-const monthEl = document.querySelector(".date h1");
-const fullDateEl = document.querySelector(".date p");
-const daysEl = document.querySelector(".days");
+// Get current date
+const currentDate = new Date();
+const currentYear = currentDate.getFullYear();
+const currentMonth = currentDate.getMonth();
+const currentDay = currentDate.getDate();
 
-const monthInx = new Date().getMonth();
-const firstDay = new Date(new Date().getFullYear(), monthInx, 1).getDay() -1;
-const lastDay = new Date(new Date().getFullYear(), monthInx +1, 0).getDate();
-
+// Months array for displaying month names
 const months = [
-    "January",
-    "February",
-    "March",
-    "April",
-    "May",
-    "June",
-    "July",
-    "August",
-    "September",
-    "October",
-    "November",
-    "December"
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December'
 ];
 
-monthEl.innerText = months[monthInx];
-fullDateEl.innerText = new Date().toDateString();
+// Function to update calendar
+function updateCalendar(year, month) {
+    const calendarTitle = document.querySelector('.months h1');
+    const calendarDate = document.querySelector('.date p');
+    const daysContainer = document.querySelector('.days');
 
-let days = "";
+    // Set the title and date
+    calendarTitle.textContent = months[month] + ' ' + year;
+    calendarDate.textContent = currentDate.toDateString();
 
-for (let i = firstDay; i > 0; i--) {
-    days += `<div class="empty"></div>`;
-}
-for (let i = 1; i<= lastDay; i++) {
-    if( i === new Date().getDate()) {
-        days += `<div class="today">${i}</div>`;
-    } else{
-    days += `<div>${i}</div>`;
+    // Clear existing days
+    daysContainer.innerHTML = '';
+
+    // Get the first day of the month
+    const firstDay = new Date(year, month, 1).getDay();
+
+    // Calculate the number of days in the month
+    const daysInMonth = new Date(year, month + 1, 0).getDate();
+
+    // Add empty divs for the days before the first day of the month
+    for (let i = 0; i < firstDay; i++) {
+        const emptyDiv = document.createElement('div');
+        emptyDiv.className = 'empty';
+        daysContainer.appendChild(emptyDiv);
+    }
+
+    // Add the days of the month
+    for (let day = 1; day <= daysInMonth; day++) {
+        const dayDiv = document.createElement('div');
+        dayDiv.textContent = day;
+        if (day === currentDay && year === currentYear && month === currentMonth) {
+            dayDiv.classList.add('today');
+        }
+        daysContainer.appendChild(dayDiv);
     }
 }
 
-daysEl.innerHTML = days;
+// Initial calendar update
+updateCalendar(currentYear, currentMonth);
